7719f905837cb0508033a655ade973705b55d128
Danbi <parkjh7219@gmail.com>
2025-11-07 16:17:07 +0900

    11

--- 10condition.py
+++ 10condition.py
@@ -19,57 +19,56 @@
 # 파이썬에서는 블록을 들여쓰기 (indent) 로 구분함
 
 # ex) 시험 점수가 85점 이상이면 '합격'이라 출력
+
+jumsu = 80
+result = '불합격입니다 !!'
+
+if jumsu >= 85:
+    result = '합격입니다 !!'
+       # result = '111'
+
+print(result)
+
+# ex 4-5
+
+eve = float(input('Enter you score : '))
+result1 = '해외 연수 기회 부여하지 않음'
+
+if eve >= 4.2:
+    result1 = '해외 연수 기회 부여'
+
+print('당신의 평점은 : ',eve)
+print(result1)
+
+# 기본 if 문
+# if 조건식 :
+#   참일 때 실행할 명령
+# else
+#   거짓일 때 실행할 명령
 #
-# jumsu = 80
-# result = '불합격입니다 !!'
-#
-# if jumsu >= 85:
-#     result = '합격입니다 !!'
-#        # result = '111'
-#
-# print(result)
-#
-# # ex 4-5
-#
-# eve = float(input('Enter you score : '))
-# result1 = '해외 연수 기회 부여하지 않음'
-#
-# if eve >= 4.2:
-#     result1 = '해외 연수 기회 부여'
-#
-# print('당신의 평점은 : ',eve)
-# print(result1)
-#
-# # 기본 if 문
-# # if 조건식 :
-# #   참일 때 실행할 명령
-# # else
-# #   거짓일 때 실행할 명령
-#
-# # ex 4-7
-#
-# age = int(input('나이를 입력하세요 :'))
-#
-# if age >= 65:
-#     result2 = '지하철 경로우대 승차권 발급'
-# else:
-#     result2 = '지하철 일반 승차권 발급'
-#
-#
-# print(result2)
-# print('자동 발매기를 이용해주셔서 감사합니다')
+# ex 4-7
+
+age = int(input('나이를 입력하세요 :'))
+
+if age >= 65:
+    result2 = '지하철 경로우대 승차권 발급'
+else:
+    result2 = '지하철 일반 승차권 발급'
+
+print(result2)
+print('자동 발매기를 이용해주셔서 감사합니다')
 
 # ex 4-8
-#
-# num1 = int(input('첫 번째 숫자 입력 : '))
-# num2 = int(input('두 번째 숫자 입력 : '))
-#
-# if num1 > num2:
-#     result = f'두 수 중에 큰 수는 {num1}이고 작은 수는 {num2}입니다.'
-# else:
-#     result = f'두 수 중에 큰 수는 {num2}이고 작은 수는 {num1}입니다.'
-#
-# print(result)
+
+num1 = int(input('첫 번째 숫자 입력 : '))
+num2 = int(input('두 번째 숫자 입력 : '))
+
+if num1 > num2:
+    result = f'두 수 중에 큰 수는 {num1}이고 작은 수는 {num2}입니다.'
+else:
+    result = f'두 수 중에 큰 수는 {num2}이고 작은 수는 {num1}입니다.'
+
+print(result)
 
 # 다중 if 문
 # if 조건식1 :
@@ -81,53 +80,53 @@
 
 # ex) 4-7 두 개의 수를 입력받아 큰 수를 출력
 # 단, 같은 수 2개를 입력했을 때
-#
-#
-# num1 = int(input('첫 번째 숫자 입력 : '))
-# num2 = int(input('두 번째 숫자 입력 : '))
-#
-# if num1 > num2:
-#     result = f'두 수 중에 큰 수는 {num1}이고 작은 수는 {num2}입니다.'
-# elif num2 > num1:
-#     result = f'두 수 중에 큰 수는 {num2}이고 작은 수는 {num1}입니다.'
-# else:
-#     result = '입력한 두 수는 같습니다'
-#
-# print(result)
+
+
+num1 = int(input('첫 번째 숫자 입력 : '))
+num2 = int(input('두 번째 숫자 입력 : '))
+
+if num1 > num2:
+    result = f'두 수 중에 큰 수는 {num1}이고 작은 수는 {num2}입니다.'
+elif num2 > num1:
+    result = f'두 수 중에 큰 수는 {num2}이고 작은 수는 {num1}입니다.'
+else:
+    result = '입력한 두 수는 같습니다'
+
+print(result)
 
 # ex 4-9
-#
-# num1 = int(input('첫 번째 숫자 입력 : '))
-# num2 = int(input('두 번째 숫자 입력 : '))
-#
-# if num1 % 2 == 0 and num2 % 2 == 0:
-#     result = '두 숫자가 모두 짝수입니다'
-# elif num1 % 2 != 0 and num2 % 2 != 0:
-#     result = '두 숫자가 모두 홀수입니다'
-# else :
-#     result = '짝수와 홀수가 섞여 있습니다'
-#
-# print(result)
+
+num1 = int(input('첫 번째 숫자 입력 : '))
+num2 = int(input('두 번째 숫자 입력 : '))
+
+if num1 % 2 == 0 and num2 % 2 == 0:
+    result = '두 숫자가 모두 짝수입니다'
+elif num1 % 2 != 0 and num2 % 2 != 0:
+    result = '두 숫자가 모두 홀수입니다'
+else :
+    result = '짝수와 홀수가 섞여 있습니다'
+
+print(result)
 
 # ex 4-10
-
+# 
 # sco = int(input('점수를 입력하세요 : '))
-#
+# 
 # if sco >= 90:
 #     result = 'A학점입니다.'
-#
+# 
 # elif sco >= 80:
 #     result = 'B학점입니다.'
-#
+# 
 # elif sco >= 70:
 #     result = 'C학점입니다.'
-#
+# 
 # elif sco >= 60:
 #     result = 'D학점입니다.'
-#
+# 
 # else:
 #     result = 'F학점입니다.'
-#
+# 
 # print(result)
 
 # 중첩 if 문 - if문 속에 또 다른 if문을 작성
@@ -142,67 +141,67 @@
 
 # ex) 두 개의 수를 입력받아 큰 수를 출력
 # 단, 음수 입력 시 양수만 입력하세요라고 하고 종료
-#
-# num1 = int(input('첫 번째 숫자 입력 : '))
-# num2 = int(input('두 번째 숫자 입력 : '))
-#
-# if num1 > 0 and num2 > 0:
-#     if num1 % 2 == 0 and num2 % 2 == 0:
-#         result = '두 숫자가 모두 짝수입니다'
-#
-#     elif num1 % 2 != 0 and num2 % 2 != 0:
-#         result = '두 숫자가 모두 홀수입니다'
-#     else :
-#         result = '짝수와 홀수가 섞여 있습니다'
-#
-# else:
-#     result = '양수만 입력하세요'
-#
-# print(result)
+
+num1 = int(input('첫 번째 숫자 입력 : '))
+num2 = int(input('두 번째 숫자 입력 : '))
+
+if num1 > 0 and num2 > 0:
+    if num1 % 2 == 0 and num2 % 2 == 0:
+        result = '두 숫자가 모두 짝수입니다'
+
+    elif num1 % 2 != 0 and num2 % 2 != 0:
+        result = '두 숫자가 모두 홀수입니다'
+    else :
+        result = '짝수와 홀수가 섞여 있습니다'
+
+else:
+    result = '양수만 입력하세요'
+
+print(result)
 
 # ex 4-10 b
 # 단 입력한 점수가 100을 초과하면
 # '점수를 잘못 입력하셨습니다!' 라고 출력하고 종료
 
-# sco = int(input('점수를 입력하세요 : '))
-#
-# if sco <= 100 :
-#     if sco >= 90 and sco > 0:
-#         result = 'A학점입니다.'
-#
-#     elif sco >= 80:
-#         result = 'B학점입니다.'
-#
-#     elif sco >= 70:
-#         result = 'C학점입니다.'
-#
-#     elif sco >= 60:
-#         result = 'D학점입니다.'
-#
-#     else:
-#         result = 'F학점입니다.'
-# else:
-#     result = '점수를 잘못 입력하셨습니다!'
-#
-# print(result)
+sco = int(input('점수를 입력하세요 : '))
+
+if sco <= 100 :
+    if sco >= 90 and sco > 0:
+        result = 'A학점입니다.'
+
+    elif sco >= 80:
+        result = 'B학점입니다.'
+
+    elif sco >= 70:
+        result = 'C학점입니다.'
+
+    elif sco >= 60:
+        result = 'D학점입니다.'
+
+    else:
+        result = 'F학점입니다.'
+else:
+    result = '점수를 잘못 입력하셨습니다!'
+
+print(result)
 
 # 문자열 비교
 # == , != : 문자열 내용과 생성된 메모리 위치 비교
 # >, <, >=, <= : 문자열을 사전순(유니코드 순서)으로 비교
-#
-# print('a' > 'A') # a : 97, A : 65
-# print('*' < 'k') # * : 42, k : 107
-# print('김' < '강') # 김 : 44608, 강 : 44053
-# print('cskim' < 'CSKIM') # c : 99, C : 67
-#
-# print('John' < 'Joho') # n : 110, o : 111
-# print('홍길덩' < '홍길딩') # 덩 : 46020, 딩 : 46308
+
+print('a' > 'A') # a : 97, A : 65
+print('*' < 'k') # * : 42, k : 107
+print('김' < '강') # 김 : 44608, 강 : 44053
+print('cskim' < 'CSKIM') # c : 99, C : 67
+
+print('John' < 'Joho') # n : 110, o : 111
+print('홍길덩' < '홍길딩') # 덩 : 46020, 딩 : 46308
 
 
 # 대소문자 구분없이 비교
-#
-# print('cskim' < 'CSKIM'.lower())
-# print('cskim'.upper() < 'CSKIM')
+
+print('cskim' < 'CSKIM'.lower())
+print('cskim'.upper() < 'CSKIM')
 
 # ex 4-14
 
@@ -211,35 +210,35 @@
 # is_valid_gender = gender == 'F' or gender == 'M'
 
 # if is_valid_gender:
-# 
-# gen = input ("성별 입력('M or m' 또는 'F or f')")
-# sta = int (input('키 입력(cm)'))
-# wei = int (input('몸무게 입력(kg)'))
-# 
-# if gen == 'M' or gen ==  'm':
-#     Rw = float(sta * sta * 22) /10000
-#     per = wei / Rw * 100
-#     if 110 <= per <= 119:
-#         result = '과체중'
-#     elif per >= 120:
-#         result = '비만 체중'
-#     else:
-#         result = '정상체중'
-# 
-# elif gen == 'F' or  gen == 'f':
-#     Rw = float(sta * sta * 21)/10000
-#     per = wei / Rw * 100
-#     if 110 <= per <= 119:
-#         result = '과체중'
-#     elif per >= 120:
-#         result = '비만 체중'
-#     else:
-#         result = '정상체중'
-# 
-# else:
-#     result = '성별 입력이 잘못 되었습니다.'
-# 
-# print(result)
+
+gen = input ("성별 입력('M or m' 또는 'F or f')")
+sta = int (input('키 입력(cm)'))
+wei = int (input('몸무게 입력(kg)'))
+
+if gen == 'M' or gen ==  'm':
+    Rw = float(sta * sta * 22) /10000
+    per = wei / Rw * 100
+    if 110 <= per <= 119:
+        result = '과체중'
+    elif per >= 120:
+        result = '비만 체중'
+    else:
+        result = '정상체중'
+
+elif gen == 'F' or  gen == 'f':
+    Rw = float(sta * sta * 21)/10000
+    per = wei / Rw * 100
+    if 110 <= per <= 119:
+        result = '과체중'
+    elif per >= 120:
+        result = '비만 체중'
+    else:
+        result = '정상체중'
+
+else:
+    result = '성별 입력이 잘못 되었습니다.'
+
+print(result) # 같은 코드를 두번 적는건 안좋은방법
 
 
 # match / case 문
@@ -279,26 +278,26 @@
 #         case _:
 #             grade = 'F학점입니다'
 
-#
-# jumsu = int(input('점수를 입력하세요 : '))
-#
-# if 0 <= jumsu <= 100:
-#     match jumsu // 10:
-#         case 100 | 9: # or 패턴
-#             result = 'A학점입니다.'
-#         case 8:
-#             result = 'B학점입니다.'
-#         case 7:
-#             result = 'C학점입니다.'
-#         case 6:
-#             result = 'D학점입니다.'
-#         case _:
-#             result = 'F학점입니다.'
-#
-# else:
-#     result = '잘못~'
-#
-# print(result)
+
+jumsu = int(input('점수를 입력하세요 : '))
+
+if 0 <= jumsu <= 100:
+    match jumsu // 10:
+        case 100 | 9: # or 패턴
+            result = 'A학점입니다.'
+        case 8:
+            result = 'B학점입니다.'
+        case 7:
+            result = 'C학점입니다.'
+        case 6:
+            result = 'D학점입니다.'
+        case _:
+            result = 'F학점입니다.'
+
+else:
+    result = '잘못~'
+
+print(result)
 
 # ex ) 1 부터 50까지 총합을 구하는 코드 작성
 
