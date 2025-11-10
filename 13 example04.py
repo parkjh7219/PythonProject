a9a41fc400c6128ece97500fbecdd5840e29b873
Danbi <parkjh7219@gmail.com>
2025-11-10 18:20:34 +0900

    11.10

--- 13 example04.py
+++ 13 example04.py
@@ -1,76 +1,101 @@
-# # sum = 0
-# #
-# # for i in range(1, 101):
-# #     if i % 3 == 0 and i % 2 != 0:
-# #         sum += i
-# #         print(i, end = ' ')
-# #
-# # print()
-# # print(sum , end = '')
-#
-# for i in range (3, 100+1, 3):
-#     if i % 2 == 0:
-#         print(i, end =' ')
-# print()
 
-# 23
+# 23 복권 - 반복문으로 재작성
 import random
 
-# 문자열 슬라이싱을 위해 생성한 난수는 문자형으로 변환
-lotto = str(random.randint(123, 789))
-# 문자열 슬라이싱을 위해 문자형으로 입력받음
-mykey = input('복권 숫자 3자리를 입력하세요 (예:123): ')
-# 상금
-prize = 0
-
-# lotto = 357    # 난수로 생성한 3자리 숫자
-# mykey = 735    # 사용자가 입력한 3자리 숫자
-
-# num1 = lotto // 100
-# num2 = (lotto % 100) // 10
-# num3 = ((lotto % 100) % 10)
+# lotto = str(random.randint(123, 789))
+# mykey = input('복권 숫자 3자리를 입력하세요 (예:123): ')
+# prize = 0
+# match = 0   # 일치수
 #
-# key1 = mykey // 100
-# key2 = (mykey % 100) // 10
-# key3 = ((mykey % 100) % 10)
+# # 개선된 코드 1
+# # for i in range(0, 3):
+# #    if lotto[i] == mykey[0] or lotto[i] == mykey[1] or lotto[i] == mykey[2]:
+# #        match += 1
 #
-# match = 0     # 일치여부
-# if num1 == key1 or num1 == key2 or num1 == key3:
-#     match += 1     # match = match + 1
-# if num2 == key1 or num2 == key2 or num2 == key3:
-#     match += 1     # match = match + 1
-# if num3 == key1 or num3 == key2 or num3 == key3:
-#     match += 1     # match = match + 1
+# # 개선된 코드 2
+# for i in range(0, 3):
+#     if lotto[i] == mykey[0]: match += 1
+#     if lotto[i] == mykey[1]: match += 1
+#     if lotto[i] == mykey[2]: match += 1
+#
+# if match == 3: prize = 1000000
+# elif match == 2: prize = 10000
+# elif match == 1: prize = 1000
+#
+# result = f'''
+# 당첨번호 : {lotto}
+# 당신의 복권번호 : {mykey}
+# 일치한 숫자 갯수 : {match}
+# 당첨 금액 : {prize}원
+# '''
+# print(result)
 
-# lotto = str(357)    # 난수로 생성한 3자리 숫자
-# mykey = str(735)    # 사용자가 입력한 3자리 숫자
 
-match = 0   # 일치수
-if lotto[0] == mykey[0] or lotto[0] == mykey[1] or lotto[0] == mykey[2]:
-    match += 1     # match = match + 1
-if lotto[1] == mykey[0] or lotto[1] == mykey[1] or lotto[1] == mykey[2]:
-    match += 1
-if lotto[2] == mykey[0] or lotto[2] == mykey[1] or lotto[2] == mykey[2]:
-    match += 1
+# 24 구구단 - 반복문으로 재작성
+# dan = int(input('출력할 구구단 단수를 입력하세요 (1-9): '))
+#
+# if 1 <= dan <= 9:
+#     result = f'=== {dan}단 ===\n'
+#     for i in range(1, 9+1):
+#         result += f'{dan} x {i} = {dan * i}\n'
+# else:
+#     result = '잘못 입력하셨습니다!!'
+#
+# print(result)
 
-if match == 3: prize = 1000000
-elif match == 2: prize = 10000
-elif match == 1: prize = 1000
 
+# 26 숫자 맞추기 - 반복문 추가
+# 반복문에서 반복 중단하려면 break 사용
+# import random
+#
+# number = random.randint(1, 100)
+# result = ''
+#
+# for _ in range(1, 25+1):
+#     mynum = int(input('1~100사이 숫자를 하나 입력하세요: '))
+#     result = '빙고! 숫자를 맞췄습니다!!'
+#     if number > mynum: result = '추측한 숫자가 작아요!'
+#     elif number < mynum: result = '추측한 숫자가 커요!'
+#     elif number == mynum: break  # 숫자를 맞추면 반복에서 중단
+#     print(result)
+#
+# print(f'{number} : {result}')
+
+
+# 30 구구단 테이블 - 반복문으로 재작성
 result = f'''
-당첨번호 : {lotto}
-당신의 복권번호 : {mykey}
-일치한 숫자 갯수 : {match}
-당첨 금액 : {prize}원
+          Multiplication Table
+      1   2   3   4   5   6   7   8   9
+---------------------------------------
 '''
+
+for i in range(1, 9+1):
+    result += f'{i} | {i*1:3d} {i*2:3d} {i*3:3d} {i*4:3d} '\
+              f'{i*5:3d} {i*6:3d} {i*7:3d} {i*8:3d} {i*9:3d}\n'
+
 print(result)
 
-# 23 복권
 
-# 24 구구단
+# 32 주민번호 검사 - 반복문으로 재작성
+jumin = '450124-1234590'
+sum = 0
 
-# 26
+# a, b
+wght = 2
+for i in range(0, 5+1):
+    #print(jumin[i], wght + i, end=', ')
+    sum += int(jumin[i]) * (wght + i) # 2,3,4,5,6,7
 
-# 30
+for i in range(7, 8+1):
+    #print(jumin[i], wght + (i - 1), end=', ')
+    sum += int(jumin[i]) * (wght + i - 1) # 8, 9
 
-# 32
+for i in range(9, 12+1):
+    #print(jumin[i], wght + (i - 9), end=', ')
+    sum += int(jumin[i]) * (wght + (i - 9)) # 2,3,4,5
+
+print(sum)
+
+# c, d
+checker = 11 - (sum % 11)
+print(checker, str(checker)[-1] == jumin[13])
