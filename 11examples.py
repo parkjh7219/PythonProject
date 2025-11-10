0e76fd4581b638b827624f8d8a88fd2c3be92852
Danbi <parkjh7219@gmail.com>
2025-11-10 13:11:43 +0900

    11.10 문풀

--- 11examples.py
+++ 11examples.py
@@ -1,88 +1,97 @@
-#21
-
-# mon = int (input('연봉을 입력하세요 (만원 단위): '))
-# mar = input('결혼 여부를 입력하세요 (y: 결혼,n: 미혼) :')
+# 21
+# salary = int(input('연봉을 입력하세요(만원 단위): '))
+# isMarried = input('결혼 여부를 입력하세요 (y:결혼, n:미혼)?').lower()
+# tax = 0
+# rate = 0
 #
-# if mar == 'y':
-#     if mon >= 6000:
-#         tax = 0.25
-#     else:
-#         tax = 0.1
-#
+# if isMarried == 'n':        # 미혼
+#     rate = 25               # 연봉 3000 이상시 세율
+#     if salary < 3000:
+#         rate = 10
+# elif isMarried == 'y':      # 기혼
+#     rate = 25               # 연봉 6000 이상시 세율
+#     if salary < 6000:
+#         rate = 10
 # else:
-#     if mon >= 3000:
-#         tax = 0.25
-#     else:
-#         tax = 0.1
+#     print('성별 오류!!')
 #
-# tlqkf = int(tax*100)
+# tax = salary * (rate / 100)
+# result = f'''
+# 적용세율 : {rate}%
+# 납부해야 할 세금은 {tax}만원입니다
+# '''
 #
-# print(tlqkf,'%')
-# print(f'납부해야 할 세금은 {mon*tax}만원입니다.')
+# print(result)
 
 
-#23
+# 23
+import random
 
-# num = int(input('복권 숫자 3자리를 입력하세요 (예 : 123) : '))
+# 문자열 슬라이싱을 위해 생성한 난수는 문자형으로 변환
+lotto = str(random.randint(123, 789))
+# 문자열 슬라이싱을 위해 문자형으로 입력받음
+mykey = input('복권 숫자 3자리를 입력하세요 (예:123): ')
+# 상금
+prize = 0
+
+# lotto = 357    # 난수로 생성한 3자리 숫자
+# mykey = 735    # 사용자가 입력한 3자리 숫자
+
+# num1 = lotto // 100
+# num2 = (lotto % 100) // 10
+# num3 = ((lotto % 100) % 10)
 #
-# import random
-# jackpot = random.randint(100, 999)
+# key1 = mykey // 100
+# key2 = (mykey % 100) // 10
+# key3 = ((mykey % 100) % 10)
 #
-# a = num // 100
-# b = (num % 100) // 10
-# c = num % 10
-#
-# x = jackpot // 100
-# y = (jackpot % 100) // 10
-# z = jackpot % 10
-#
-# if a == x :
-#     ax = 1
-# else:
-#     ax = 0
-#
-# if b == y :
-#     by = 1
-# else:
-#     by = 0
-#
-# if c == z :
-#     cz = 1
-# else:
-#     cz = 0
-#
-# num = ax+by+cz
-# if num > 0:
-#     match num:
-#         case 3:
-#             result = '당첨 금액 : 100만원'
-#         case 2:
-#             result = '당첨 금액 : 만원'
-#         case 1:
-#             result = '당첨 금액 : 천원'
-# else:
-#     result = '당첨되지 못했습니다.'
-#
-# print (f'당첨번호 : {jackpot}')
-# print (num)
-# print (result)
+# match = 0     # 일치여부
+# if num1 == key1 or num1 == key2 or num1 == key3:
+#     match += 1     # match = match + 1
+# if num2 == key1 or num2 == key2 or num2 == key3:
+#     match += 1     # match = match + 1
+# if num3 == key1 or num3 == key2 or num3 == key3:
+#     match += 1     # match = match + 1
+
+# lotto = str(357)    # 난수로 생성한 3자리 숫자
+# mykey = str(735)    # 사용자가 입력한 3자리 숫자
+
+match = 0   # 일치수
+if lotto[0] == mykey[0] or lotto[0] == mykey[1] or lotto[0] == mykey[2]:
+    match += 1     # match = match + 1
+if lotto[1] == mykey[0] or lotto[1] == mykey[1] or lotto[1] == mykey[2]:
+    match += 1
+if lotto[2] == mykey[0] or lotto[2] == mykey[1] or lotto[2] == mykey[2]:
+    match += 1
+
+if match == 3: prize = 1000000
+elif match == 2: prize = 10000
+elif match == 1: prize = 1000
+
+result = f'''
+당첨번호 : {lotto}
+당신의 복권번호 : {mykey}
+일치한 숫자 갯수 : {match} 
+당첨 금액 : {prize}원 
+'''
+print(result)
 
 
+# 26
+import random
+
+# 1~100 사이 임의의 난수 생성
+number = random.randint(1, 100)
+# 사용자로부터 값을 하나 입력 받음
+mynum = int(input('1~100사이 숫자를 하나 입력하세요: '))
+
+result = '빙고! 숫자를 맞췄습니다!!'
+if number > mynum: result = '추측한 숫자가 작아요!'
+elif number < mynum: result = '추측한 숫자가 커요!'
+
+print(f'{number} : {result}')
 
 
-# match = 0
-# a == x or a == y or a == z
-# match += 1
-# b == x or b == y or b == z
-# match += 1
-# c == x or c == y or c == z
-# match += 1
+# 31
 
-# print(match)
-
-
-#26
-
-#31
-
-#32
+# 32
